2e3051fd9bc73564c8c699d1424c5e4f0704329f
vishalshep08 <vishalrameshshep0809@gmail.com>
2025-01-10 15:11:39 +0530

    Commit

---
+++ src/StringOperation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringOperation{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.next();
+        System.out.println("The string is:"+str);
+        for( int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            //int ascii = (int) c;
+            //System.out.println("The ASCII value of the '"+c+"' is:"+ascii);
+            //Bitwise AND
+            int and = 127 & c;
+            System.out.println("The AND of the character '"+c+"' is:"+ and);
+
+            //Bitwise OR
+            int or = 127 ^ c;
+            System.out.println("The XOR of the character '"+c+"' is:"+ or);
+        }
+    }
+}
