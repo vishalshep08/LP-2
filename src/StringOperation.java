e4f3fdc7d8a31c914fd76cffcbf3e5b5cc9d803d
vishalshep08 <145660573+vishalshep08@users.noreply.github.com>
2025-01-15 22:37:46 +0530

    Commit

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -8,7 +8,7 @@ public class StringOperation{
         System.out.println("The string is:"+str);
         for( int i = 0; i < str.length(); i++){
             char c = str.charAt(i);
-            // For displaying the ASCII code
+            //For displaying the ASCII code
             //int ascii = (int) c;
             //System.out.println("The ASCII value of the '"+c+"' is:"+ascii);
             //Bitwise AND
