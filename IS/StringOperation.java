7757e07ff2fa13d34e83737ffdcbeea651991d5d
vishalshep08 <145660573+vishalshep08@users.noreply.github.com>
2025-04-09 11:25:22 +0530

    commit

--- IS/StringOperation.java
+++ IS/StringOperation.java
@@ -8,9 +8,6 @@ public class StringOperation{
         System.out.println("The string is:"+str);
         for( int i = 0; i < str.length(); i++){
             char c = str.charAt(i);
-            //For displaying the ASCII code
-            //int ascii = (int) c;
-            //System.out.println("The ASCII value of the '"+c+"' is:"+ascii);
             //Bitwise AND
             int and = 127 & c;
             System.out.println("The AND of the character '"+c+"' is:"+ and);
